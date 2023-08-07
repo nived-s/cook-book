f40bace6e155e8753c49f6642731b2a639419b5e
nived-s <nivedsreenilayam@gmail.com>
2023-08-07 21:10:45 +0530

    onboarding all screens ui done

--- cook_book/lib/screens/onBoarding/onboarding_screen.dart
+++ cook_book/lib/screens/onBoarding/onboarding_screen.dart
@@ -4,9 +4,7 @@ import '../../utilities/constants.dart';
 
 // pages
 import 'package:cook_book/screens/home_screen.dart';
-import 'package:cook_book/screens/onBoarding/screen1.dart';
-import 'package:cook_book/screens/onBoarding/screen2.dart';
-import 'package:cook_book/screens/onBoarding/screen3.dart';
+import 'package:cook_book/screens/onBoarding/onBoardingWidget.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
@@ -29,91 +27,112 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
         child: Stack(
           children: [
             // scrolling pages
-            PageView(
-              onPageChanged: (index) {
-                setState(() {
-                  onLastPage = (index == 2);
-                });
-              },
-              controller: _controller,
-              children: [
-                OnBoarding1(),
-                OnBoarding1(),
-                OnBoarding1(),
-              ],
-            ),
-            // smooth page indicator
             Container(
-              alignment: const Alignment(0, 0.75),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  // skip button
-                  GestureDetector(
-                    onTap: () {
-                      _controller.jumpToPage(2);
-                    },
-                    child: Text(
-                      'Skip',
-                      style: kOnboadingSkipBtn,
-                    ),
+              padding: const EdgeInsets.only(bottom: 80),
+              child: PageView(
+                onPageChanged: (index) {
+                  setState(() {
+                    onLastPage = (index == 2);
+                  });
+                },
+                controller: _controller,
+                children: const [
+                  OnBoarding(
+                    gradRotation: 0.4,
+                    image: 'images/onboard5.png',
+                    headingText: 'Welcome to CookBook',
+                    descriptionText:
+                        'Experience the joy of cooking through a seamless blend of written recipes and engaging video guides.',
                   ),
-                  // page indicator
-                  SmoothPageIndicator(
-                    controller: _controller,
-                    count: 3,
-                    effect: const SwapEffect(
-                      dotColor: kSecondaryColour,
-                      activeDotColor: kTeritiaryColour,
-                    ),
+                  OnBoarding(
+                    gradRotation: -0.4,
+                    image: 'images/onboard6.png',
+                    headingText: 'Discover a World of Flavors',
+                    descriptionText:
+                        'From traditional favorites to innovative twists, there\'s a recipe for every craving.Explore global cuisines and uncover the secrets of diverse flavors and ingredients.',
                   ),
-                  // next or done btn
-                  onLastPage
-                      ? Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: kTeritiaryColour,
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => HomeScreen(),
-                                    ));
-                              },
-                              child: Text(
-                                'Done',
-                                style: kOnboadingNextDoneBtn,
-                              ),
-                            ),
-                          ),
-                        )
-                      : Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: kTeritiaryColour,
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: GestureDetector(
-                              onTap: () {
-                                _controller.nextPage(
-                                    duration: const Duration(milliseconds: 500),
-                                    curve: Curves.easeInSine);
-                              },
-                              child: Text(
-                                'Next',
-                                style: kOnboadingNextDoneBtn,
-                              ),
-                            ),
-                          ),
-                        ),
+                  OnBoarding(
+                    gradRotation: 0.4,
+                    image: 'images/onboard7.png',
+                    headingText: 'Create, Cook, and Share',
+                    descriptionText:
+                        'Join a community that celebrates the joy of cooking and sharing.Elevate your recipes with captivating video guides.',
+                  ),
                 ],
               ),
             ),
+          ],
+        ),
+      ),
+      // smooth page indicator
+      bottomSheet: SizedBox(
+        height: 80,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // skip button
+            GestureDetector(
+              onTap: () {
+                _controller.jumpToPage(2);
+              },
+              child: Text(
+                'Skip',
+                style: kOnboadingSkipBtn,
+              ),
+            ),
+            // page indicator
+            SmoothPageIndicator(
+              controller: _controller,
+              count: 3,
+              effect: const SwapEffect(
+                dotColor: kSecondaryColour,
+                activeDotColor: kTeritiaryColour,
+              ),
+            ),
+            // next or done btn
+            onLastPage
+                ? Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: kTeritiaryColour,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => HomeScreen(),
+                              ));
+                        },
+                        child: Text(
+                          'Done',
+                          style: kOnboadingNextDoneBtn,
+                        ),
+                      ),
+                    ),
+                  )
+                : Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: kTeritiaryColour,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          _controller.nextPage(
+                              duration: const Duration(milliseconds: 500),
+                              curve: Curves.easeInSine);
+                        },
+                        child: Text(
+                          'Next',
+                          style: kOnboadingNextDoneBtn,
+                        ),
+                      ),
+                    ),
+                  ),
           ],
         ),
       ),

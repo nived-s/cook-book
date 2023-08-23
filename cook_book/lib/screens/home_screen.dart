eb09474711cde55797ab9d3d328dfe75df4e875e
nived-s <nivedsreenilayam@gmail.com>
2023-08-23 22:57:30 +0530

    home screen ui done

--- cook_book/lib/screens/home_screen.dart
+++ cook_book/lib/screens/home_screen.dart
@@ -6,7 +6,67 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('🏠Home page', style: TextStyle(fontSize: 52,),)),
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.yellow[100]!, Colors.white],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Let's cook something today",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search recipes...",
+                    prefixIcon: const Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                "Try searching for 'Pasta' or 'Chocolate cake'",
+                style: TextStyle(color: Colors.grey),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Search"),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Image.asset(
+                'images/cook.png',
+                width: 350,
+                height: 250,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
